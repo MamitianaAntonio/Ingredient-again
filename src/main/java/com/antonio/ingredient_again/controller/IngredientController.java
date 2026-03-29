e9ea5fd4ec05cb4a6b1a351e56f47f80c6c70cf7
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-29 21:19:20 +0300

    feat(controller): add GET /ingredients/{id} endpoint with manual 404 handling

--- src/main/java/com/antonio/ingredient_again/controller/IngredientController.java
+++ src/main/java/com/antonio/ingredient_again/controller/IngredientController.java
@@ -1,11 +1,12 @@
 package com.antonio.ingredient_again.controller;
 
 import com.antonio.ingredient_again.dto.IngredientResponse;
-import com.antonio.ingredient_again.entity.ingredient.Ingredient;
+import com.antonio.ingredient_again.exception.BadRequestException;
 import com.antonio.ingredient_again.service.IngredientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,17 @@ public class IngredientController {
     public ResponseEntity<List<IngredientResponse>> getIngredients () {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(service.getAllIngredients());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getIngredientById(@PathVariable Integer id) {
+        try {
+            IngredientResponse ingredientResponse = service.getIngredientById(id);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ingredientResponse);
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
     }
 }
