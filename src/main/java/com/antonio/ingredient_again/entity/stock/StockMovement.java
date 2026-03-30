9d297aa9f88a6a25892057e51a1b9de532bf5607
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-30 14:41:02 +0300

    feat(stock): update stock entity classes

--- src/main/java/com/antonio/ingredient_again/entity/stock/StockMovement.java
+++ src/main/java/com/antonio/ingredient_again/entity/stock/StockMovement.java
@@ -7,4 +7,53 @@ public class StockMovement {
     private StockValue value;
     private MovementTypeEnum type;
     private Instant creationDataTime;
+
+    public StockMovement(Integer id, StockValue value, MovementTypeEnum type, Instant creationDataTime) {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+        this.creationDataTime = creationDataTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public StockValue getValue() {
+        return value;
+    }
+
+    public void setValue(StockValue value) {
+        this.value = value;
+    }
+
+    public MovementTypeEnum getType() {
+        return type;
+    }
+
+    public void setType(MovementTypeEnum type) {
+        this.type = type;
+    }
+
+    public Instant getCreationDataTime() {
+        return creationDataTime;
+    }
+
+    public void setCreationDataTime(Instant creationDataTime) {
+        this.creationDataTime = creationDataTime;
+    }
+
+    @Override
+    public String toString() {
+        return "StockMovement{" +
+                "id=" + id +
+                ", value=" + value +
+                ", type=" + type +
+                ", creationDataTime=" + creationDataTime +
+                '}';
+    }
 }
--- src/main/java/com/antonio/ingredient_again/entity/stock/StockValue.java
+++ src/main/java/com/antonio/ingredient_again/entity/stock/StockValue.java
@@ -5,4 +5,33 @@ import com.antonio.ingredient_again.entity.ingredient.UnitEnum;
 public class StockValue {
     private Double quantity;
     private UnitEnum unit;
+
+    public StockValue(Double quantity, UnitEnum unit) {
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public UnitEnum getUnit() {
+        return unit;
+    }
+
+    public void setUnit(UnitEnum unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "StockValue{" +
+                "quantity=" + quantity +
+                ", unit=" + unit +
+                '}';
+    }
 }
